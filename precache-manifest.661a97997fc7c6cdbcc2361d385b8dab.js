7ba1d22a6ec1845bf17da529aa77100b654a56fa
Prateek Rawat <prateekrawat33@gmail.com>
2020-08-13 03:17:56 +0530

    Updates

---
+++ precache-manifest.661a97997fc7c6cdbcc2361d385b8dab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba5c60f4a906f47981871ae35c30942",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "bdc7b890c4cc1a80b80d",
+    "url": "/covid-19-tracker/static/css/main.9eb8d374.chunk.css"
+  },
+  {
+    "revision": "039a4d4d7e3ec1893ef2",
+    "url": "/covid-19-tracker/static/js/2.33e421f3.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-19-tracker/static/js/2.33e421f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdc7b890c4cc1a80b80d",
+    "url": "/covid-19-tracker/static/js/main.84f13796.chunk.js"
+  },
+  {
+    "revision": "81ea5990077629c9a558",
+    "url": "/covid-19-tracker/static/js/runtime-main.ac4af076.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
